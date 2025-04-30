108c3be0712d6d2a69976efd1dd08f0e8eba39f0
Code Review Guru <113462727+blvkhakr@users.noreply.github.com>
2025-04-30 09:40:18 -0500

    Create testing.py

---
+++ Snyk Scripts/testing.py
@@ -0,0 +1,59 @@
+import requests, json
+from config import SNYK_TOKEN, GITHUB_TOKEN, SNYK_API_BASE, GITHUB_API_BASE, SNYK_REST_API_BASE, HEADERS_SNYK
+from snyk_api import get_orgs, get_projects
+from utils import log
+
+# TEST Snyk API
+def test_snyk():
+    #res = requests.get(f"{SNYK_API_BASE}/orgs", headers=HEADERS_SNYK, verify=False)
+    #res = requests.get(f"{SNYK_REST_API_BASE}/orgs?version=2024-10-15", headers=HEADERS_SNYK, verify=False)
+    res = requests.get(f"https://api.snyk.io/rest/orgs?version=2024-10-15", headers=headers, verify=False)
+    org_data = json.loads(res.text)
+   
+    print(org_data)
+    print(type(org_data))
+    if res.ok:
+        print("hello")
+        #print(f"[SNYK Auth success: Found {len(res.json().get('orgs', []))} org(s)")
+       # print(f"[SNYK Auth success: Found {len(org_data.)}")
+                                          #     json().get('orgs', []))} org(s)")
+    else:
+       print(f"[SNYK Auth Failed: Found {res.status_code} - {res.text}")
+
+# Test Github API
+def test_github():
+    headers = {"Authorization": f"token {GITHUB_TOKEN}"}
+    res = requests.get(f"{GITHUB_API_BASE}/user", headers=headers)
+    if res.ok:
+        print(f"[GitHub Auth success: Logged in as {res.json().get('login')}")
+    else:
+       print(f"[GitHub Auth Failed: Found {res.status_code} - {res.text}")
+
+
+def test_orgs_projects():
+    log("Testing connection to Snyk....")
+
+    # Step 1: Get all orgs
+    orgs = get_orgs()
+    if not orgs:
+        log("[ SNYK ] No organization found. Check your token.")
+        return log (f"[ SNYK ] Found {len(orgs)} organization(s).")
+    for org in orgs:
+        log(f"Org: {org.get('name')} (ID: {org.get('id')}")
+    
+    # Step 2: Test pulling projects from the first org
+    first_org = orgs[0]
+    org_id = first_org.get('id')
+
+    projects = get_projects(org_id)
+    if not projects: 
+        log(f" [ SNYK ] No projects found in organization {first_org.get('name')}")    
+        return log(f"[ SNYK ] Found {len(projects)} project(s) in {first_org.get('name')}.")          
+    for project in projects[:5]:
+        # Limit to first 5 for quick display
+        log(f"Project: {project.get('name')} (ID: {project.get('id')})")                  
+
+
+if __name__ == "__main__":
+   #test_orgs_projects()
+   test_snyk()
